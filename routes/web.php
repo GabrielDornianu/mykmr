0e37dec4057bc22452e90c672de85d192e6492f5
gabrieldornianu <gabrieldornianu@yahoo.com>
2018-03-13 23:14:12 +0200

    Created the admin route and middleware

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin', 'middleware' => 'checkadmin'], function(){
+  Route::get('/', [
+    'uses' => 'AdminController@index',
+    'as'   => 'index'
+  ]);
+});
